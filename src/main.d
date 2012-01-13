f21862366a6d355edd30c892fec6ecb487bedf2d
Aramande <aramande@hackular.com>
2012-01-13 14:50:24 +0100

    Renamed vector variables and implemented initial dsss script

--- src/main.d
+++ src/main.d
@@ -1,3 +1,5 @@
+module main;
+
 import dhe.util.geometry.all;
 import std.stdio;
 
@@ -5,6 +7,6 @@ void main(string args[])
 {
 	Vector vec = Vector(3, 1);
 
-	writefln("Vector: %s", vec.normalize());
+	writefln("Vector: %s, Point: %s", vec, vec.normalize().getTarget());
 	readln();
 }
